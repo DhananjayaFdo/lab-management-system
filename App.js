8bc23167434a88f3b128088514927eef2c23aa2d
Dhananjaya Fdo <136318280+DhananjayaFdo@users.noreply.github.com>
2024-03-12 09:57:43 +0530

    push

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+
 
 export default function App() {
+
+  const [isDoctor, setDoctor] = useState(false);
+
+  function click(){
+    setDoctor(!isDoctor);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isDoctor ? 
+        (
+          <View style={styles.doctor}>
+            <View>
+              <Button
+                onPress={click}
+                title="change to user"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+              />
+            </View>
+          </View>
+        ) :
+        ( 
+          <View style={styles.user}>
+            <Button
+                onPress={click}
+                title="change to doctor"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+              />
+          </View>
+        )
+      }
     </View>
   );
 }
@@ -13,8 +43,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+
+  doctor: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  user:{
+    flex: 1,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+  });
+
+
